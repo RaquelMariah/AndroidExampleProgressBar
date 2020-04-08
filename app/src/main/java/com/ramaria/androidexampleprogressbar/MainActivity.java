5270ceed6b5a04ea2ffbe571d9a1188a0d9c2ff7
Raquel Maria <45218570+RaquelMariah@users.noreply.github.com>
2020-04-08 19:03:27 -0300

    Adicionadas progressBars

--- app/src/main/java/com/ramaria/androidexampleprogressbar/MainActivity.java
+++ app/src/main/java/com/ramaria/androidexampleprogressbar/MainActivity.java
@@ -3,12 +3,35 @@ package com.ramaria.androidexampleprogressbar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+    protected ProgressBar progressBarHorizontal;
+    protected ProgressBar progressBarCircular;
+    protected int progresso = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    progressBarHorizontal = findViewById(R.id.progressBarHorizontal);
+    progressBarCircular = findViewById(R.id.progressBarCircular);
+
+    progressBarCircular.setVisibility(View.GONE);
+
     }
+    public void carregarProgressBar(View view){
+
+        this.progresso = this.progresso+1;
+        progressBarHorizontal.setProgress(this.progresso);
+
+        progressBarCircular.setVisibility(View.VISIBLE);
+
+        if(this.progresso == 10){
+            progressBarCircular.setVisibility(View.GONE);
+        }
+
+    }
+
 }
